5eada20f7f71c01826e472ee267c807cbe4ade3b
Henrik Haugbølle <henrikhaugboelle@live.dk>
2012-01-25 22:14:45 +0100

    pushing to give an overview

--- app/code/controllers/Member/LinkController.php
+++ app/code/controllers/Member/LinkController.php
@@ -1,16 +1,14 @@
 <?php
-
-include 'app/code/modules/Link.php';
-include 'app/code/modules/Link/Load.php';
-include 'app/code/modules/Link/DAO.php';
-include 'app/code/modules/Link/ValueObject.php';
-
 class Member_LinkController extends ENT_Controller {
 	public function viewAction() {
 		$links = ENT::getCollection('link');
 		$links->build();
 
 		ENT::register('links', $links);
+	}
+
+	public function itemAction() {
+		$this->renderLayout(false);
 	}
 }
 ?>
